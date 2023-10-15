f97b0ee4d0d0c289ae9eef92cb37aa6bc0c61c5c
SyedMuhammadArsalanShah <smarsalanshah125@gmail.com>
2023-10-15 19:42:57 +0500

    lect 01 to lect 03

--- lecture03crudraw.php
+++ lecture03crudraw.php
@@ -1,18 +1,8 @@
 <?php
-
-
-
 $server = "localhost";
-
 $username = "root";
-
-
-
 $password = "";
-
-
 $con = mysqli_connect($server, $username, $password, "kinza");
-
 
 if (!$con) {
     die("connection failed " . mysqli_connect_error());
@@ -20,139 +10,61 @@ if (!$con) {
     echo "database is success";
 }
 
+// Database creation
+$meraSql = "Create database kinza";
+$res = mysqli_query($con, $meraSql);
 
+if ($res) {
+    echo "db is created ";
+}
 
+// Table creation
+$table = "
+ create table info 
+ ( id int primary key  not null auto_increment , 
+ name varchar(50), 
+ email varchar(50),
+ pass varchar(50)
+ )
+";
 
-// $meraSql = "Create database kinza";
+$res = mysqli_query($con, $table);
 
+if ($res) {
+    echo "table is created ";
+}
 
-// $res = mysqli_query($con, $meraSql);
-
-
-
-// if ($res) {
-
-//     echo "db is created ";
-// }
-
-
-
-
-
-
-// $table = "
-//  create table info 
-//  ( id int primary key  not null auto_increment , 
-
-//  name varchar(50), 
-//  email varchar(50),
-//  pass varchar(50)
-
-//  )
-
-// ";
-
-
-
-
-// $res = mysqli_query($con, $table);
-
-
-// if ($res) {
-//     echo "table is created ";
-// }
-
-
-
-
-
-
-
-
-
+// Insert query
 $name = "chintumintu";
 $email = "cm@gmail.com";
 $pass = "123456";
-
-
-
-
-
-
-$insert = "insert into info (name, email, pass)values('$name', '$email', '$pass')";
-
-
-
-
-
+$insert = "insert into info (name, email, pass) values('$name', '$email', '$pass')";
 $res = mysqli_query($con, $insert);
 
-
-
-
-
 if ($res) {
-    # code...
-
     echo "code inserted";
 }
 
-
-
-
-
-
-
+// Read data query
 $sql = "Select * from info";
-
-
-
-
 $res = mysqli_query($con, $sql);
 
-
-
-
-
+// Count db table records in a table
 $mytotalrows = mysqli_num_rows($res);
+echo "my total rows = " . $mytotalrows . "<br>";
 
+// Get each record of the table
+$row = mysqli_fetch_assoc($res);
+echo "<br> this is my row  " . var_dump($row);
 
-
-
-echo "my total rows =" . $mytotalrows . "<br>";
-
-
-
-
-
-
-
-
-// $row = mysqli_fetch_assoc($res);
-
-// echo "<br> this my row  " . var_dump($row);
-
-
-
-// $row = mysqli_fetch_assoc($res);
-
-// echo "<br> this my row  " . var_dump($row);
-
-
+$row = mysqli_fetch_assoc($res);
+echo "<br> this is my row  " . var_dump($row);
 
 if ($mytotalrows > 0) {
-
-
     while ($row = mysqli_fetch_assoc($res)) {
-
-        // echo "<br> this my row  " . var_dump($row) . "<br>";
-   
-        echo "Name = ".$row["name"]."<br>";
-        echo "email = ".$row["email"]."<br>";
-        echo "pass = ".$row["pass"]."<br>";
- 
-   
-   
+        echo "Name = " . $row["name"] . "<br>";
+        echo "email = " . $row["email"] . "<br>";
+        echo "pass = " . $row["pass"] . "<br>";
     }
 }
 ?>
